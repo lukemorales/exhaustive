bbaba375fecaec9bb1977fcaa73e676b05b7e166
Luke Morales <lukemorales@live.com>
2023-01-31 23:16:25 -0300

    Add tagged union overload to main function

--- src/exhaustive.ts
+++ src/exhaustive.ts
@@ -2,6 +2,16 @@ import { corrupt } from './corrupt';
 
 type AnyFunction = (...args: any[]) => unknown;
 
+type ExhaustiveUnion<Union extends string> = {
+  [Key in Union]: (value: Key) => any;
+} & ExhaustiveFallback;
+
+type ExhaustiveTag<Union extends object, Tag extends keyof Union> = {
+  [Key in Union[Tag] & string]: (
+    value: Extract<Union, { [K in Tag]: Key }>,
+  ) => any;
+} & ExhaustiveFallback;
+
 type ExhaustiveFallback = {
   /**
    * Default fallback
@@ -22,53 +32,54 @@ type ValidateKeys<T, U> = [keyof T] extends [keyof U]
       [Key in keyof U]: Key extends keyof T ? T[Key] : never;
     };
 
-interface Exhaustive {
-  <
-    Union extends string,
-    Match extends ExhaustiveUnion<Union> = ExhaustiveUnion<Union>,
-  >(
-    union: Union,
-    match: ValidateKeys<Match, ExhaustiveUnion<Union>>,
-  ): Match[keyof Match] extends AnyFunction
-    ? ReturnType<Match[keyof Match]>
-    : never;
-  _tag: <
-    Union extends object,
-    Tag extends keyof Union,
-    Match extends ExhaustiveTag<Union, Tag> = ExhaustiveTag<Union, Tag>,
-  >(
-    union: Union,
-    tag: Tag,
-    match: ValidateKeys<Match, ExhaustiveTag<Union, Tag>>,
-  ) => Match[keyof Match] extends AnyFunction
-    ? ReturnType<Match[keyof Match]>
-    : never;
-}
+function exhaustive<
+  Union extends string,
+  Match extends ExhaustiveUnion<Union> = ExhaustiveUnion<Union>,
+>(
+  union: Union,
+  match: ValidateKeys<Match, ExhaustiveUnion<Union>>,
+): Match[keyof Match] extends AnyFunction
+  ? ReturnType<Match[keyof Match]>
+  : never;
+function exhaustive<
+  Union extends object,
+  Tag extends keyof Union,
+  Match extends ExhaustiveTag<Union, Tag> = ExhaustiveTag<Union, Tag>,
+>(
+  union: Union,
+  tag: Tag,
+  match: ValidateKeys<Match, ExhaustiveTag<Union, Tag>>,
+): Match[keyof Match] extends AnyFunction
+  ? ReturnType<Match[keyof Match]>
+  : never;
+function exhaustive(union: any, matchOrKeyofUnion: any, match?: any) {
+  if (typeof match !== 'undefined') {
+    return exhaustive._tag(union, matchOrKeyofUnion, match);
+  }
 
-type ExhaustiveUnion<Union extends string> = {
-  [Key in Union]: (value: Key) => any;
-} & ExhaustiveFallback;
-
-type ExhaustiveTag<Union extends object, Tag extends keyof Union> = {
-  [Key in Union[Tag] & string]: (
-    value: Extract<Union, { [K in Tag]: Key }>,
-  ) => any;
-} & ExhaustiveFallback;
-
-export const exhaustive: Exhaustive = (union, match) => {
-  if (!Object.prototype.hasOwnProperty.call(match, union)) {
-    if (Object.prototype.hasOwnProperty.call(match, '_')) {
-      return (match as Required<ExhaustiveFallback>)._();
+  if (!Object.prototype.hasOwnProperty.call(matchOrKeyofUnion, union)) {
+    if (Object.prototype.hasOwnProperty.call(matchOrKeyofUnion, '_')) {
+      return (matchOrKeyofUnion as Required<ExhaustiveFallback>)._();
     }
 
     return corrupt(union as never);
   }
 
-  const result = match[union as string];
+  const result = (matchOrKeyofUnion as object)[union as string];
   return result(union);
-};
+}
 
-exhaustive._tag = (union, tag, match) => {
+exhaustive._tag = <
+  Union extends object,
+  Tag extends keyof Union,
+  Match extends ExhaustiveTag<Union, Tag> = ExhaustiveTag<Union, Tag>,
+>(
+  union: Union,
+  tag: Tag,
+  match: ValidateKeys<Match, ExhaustiveTag<Union, Tag>>,
+): Match[keyof Match] extends AnyFunction
+  ? ReturnType<Match[keyof Match]>
+  : never => {
   const key = union[tag];
 
   if (!Object.prototype.hasOwnProperty.call(match, key)) {
@@ -82,3 +93,5 @@ exhaustive._tag = (union, tag, match) => {
   const result = match[key as string];
   return result(union);
 };
+
+export { exhaustive };
